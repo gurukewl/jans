451bd8813f74ebaad05f0b87b8d92b7f634b4a7d
Arijit Bhattacharyya <1336718+gurukewl@users.noreply.github.com>
2023-05-25 12:32:35 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -53,12 +53,6 @@ if [ "$CHANGE_HOSTNAME" == "y" ]; then
     host_name=${host_name:-"jans"}
 fi
 
-LINUX=$(lsb_release -a 2>/dev/null | grep Distributor | sed "s/Distributor ID:\t//")
-RELEASE=$(lsb_release -a 2>/dev/null | grep Codename | sed "s/Codename:\t//")
-
-echo "Linux is $LINUX"
-echo "Release is $RELEASE"
-
 check_dependencides() {
     if command -v $1 &> /dev/null; then
         send_success_message "$1 exists ✅ "
